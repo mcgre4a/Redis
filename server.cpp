6c935bc6f69ebd75b592bfc277dc43bfe7c78c1d
mcgre4a <artur270605@gmail.com>
2024-08-16 12:20:14 +0300

    4th stage, accepting several requests, EOF bug?..

--- server.cpp
+++ server.cpp
@@ -7,8 +7,12 @@
 #include <cerrno>
 #include <cstdlib>
 #include <cstdio>
+#include <cassert>
 
-static void msg(const char *string);
+
+static void msg(const char *string) {
+    std::cout << string;
+}
 
 void die(const char *message) {
     perror(message);  // Print the error message associated with the failed system call
@@ -28,13 +32,82 @@ static void do_something(int connfd) {
     write(connfd, wbuf, strlen(wbuf));
 }
 
-static void msg(const char *string) {
-    std::cout << string;
+
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error, or unexpected EOF
+        }
+        assert((size_t) rv <= n);
+        n -= (size_t) rv;
+        buf += rv;
+    }
+    return 0;
 }
 
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error
+        }
+        assert((size_t) rv <= n);
+        n -= (size_t) rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+const size_t k_max_msg = 4096;
+
+static int32_t one_request(int connfd) {
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err) {
+        if (errno == 0) {
+            msg("EOF");
+        } else {
+            msg("read() error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);  // assume little endian
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    // request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    // do something
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);
+
+    // reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t) strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
+}
 
 int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        die("socket()");
+    }
+
     int val = 1;
     setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
     std::cout << "Hello, World!" << std::endl;
@@ -60,6 +133,14 @@ int main() {
         if (connfd < 0) {
             continue;   // error
         }
+        // only serves one client connection at once
+        while (true) {
+            int32_t err = one_request(connfd);
+            if (err) {
+                break;
+            }
+        }
+        close(connfd);
 
         do_something(connfd);
         close(connfd);
